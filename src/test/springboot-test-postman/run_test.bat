39387739a9bcee3d79d7362229747ed1b0389fb5
Sebastian Zapata <zapatasebastian001@gmail.com>
2025-04-27 22:31:14 -0500

    refactoring workflow
    Se actualiza el archivo yml y archivo bat

--- src/test/springboot-test-postman/run_test.bat
+++ src/test/springboot-test-postman/run_test.bat
@@ -1,10 +1,9 @@
 @echo off
-rem Obtener la fecha y hora en formato YYYYMMDD_HHMM
 for /f "tokens=2 delims==" %%I in ('"wmic os get localdatetime /value"') do set datetime=%%I
 set fecha=%datetime:~0,4%%datetime:~4,2%%datetime:~6,2%_%datetime:~8,2%%datetime:~10,2%
 
-rem Ejecutar Newman con los archivos dentro del repositorio
-newman run "src/test/springboot-test-postman/SpringBoot Tests.postman_collection.json" ^
--e "src/test/springboot-test-postman/localhost.postman_environment.json" ^
+:: Usar rutas relativas para apuntar a los archivos
+newman run "%~dp0SpringBoot Tests.postman_collection.json" ^
+-e "%~dp0localhost.postman_environment.json" ^
 --reporters cli,htmlextra ^
---reporter-htmlextra-export "src/test/springboot-test-postman/reporte_%fecha%.html"
+--reporter-htmlextra-export "%~dp0reporte_%fecha%.html"
