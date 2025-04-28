0670c1a62547494992d01f63bd23be898d4c87d4
Sebastian Zapata <zapatasebastian001@gmail.com>
2025-04-27 19:51:43 -0500

    run test from workflow
    Se sube el workflow para ejecutar los test, y se modifica el bat para las rutas del repositorio de gitHub

--- src/test/springboot-test-postman/run_test.bat
+++ src/test/springboot-test-postman/run_test.bat
@@ -1,8 +1,10 @@
 @echo off
+rem Obtener la fecha y hora en formato YYYYMMDD_HHMM
 for /f "tokens=2 delims==" %%I in ('"wmic os get localdatetime /value"') do set datetime=%%I
 set fecha=%datetime:~0,4%%datetime:~4,2%%datetime:~6,2%_%datetime:~8,2%%datetime:~10,2%
 
-newman run "C:\Users\zapat\Documents\Cursos\GitHub_Actions\test_colection\SpringBoot Tests.postman_collection.json" ^
--e "C:\Users\zapat\Documents\Cursos\GitHub_Actions\test_colection\localhost.postman_environment.json" ^
+rem Ejecutar Newman con los archivos dentro del repositorio
+newman run "src/test/springboot-test-postman/SpringBoot Tests.postman_collection.json" ^
+-e "src/test/springboot-test-postman/localhost.postman_environment.json" ^
 --reporters cli,htmlextra ^
---reporter-htmlextra-export "C:\Users\zapat\Documents\Cursos\GitHub_Actions\test_colection\reporte_%fecha%.html"
+--reporter-htmlextra-export "src/test/springboot-test-postman/reporte_%fecha%.html"
