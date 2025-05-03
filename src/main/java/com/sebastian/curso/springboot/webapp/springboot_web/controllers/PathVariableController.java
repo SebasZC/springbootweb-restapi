02cc529c463568c8dba64052858d020cdc0d9039
Sebastian Zapata <zapatasebastian001@gmail.com>
2025-05-03 17:45:37 -0500

    Config values UTF8
    Se configura el archivo para acentuaciones con propiedad encoding dentro de valuesConfig.java

--- src/main/java/com/sebastian/curso/springboot/webapp/springboot_web/controllers/PathVariableController.java
+++ src/main/java/com/sebastian/curso/springboot/webapp/springboot_web/controllers/PathVariableController.java
@@ -9,6 +9,7 @@ import com.sebastian.curso.springboot.webapp.springboot_web.models.User;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,6 +21,19 @@ import org.springframework.web.bind.annotation.RequestBody;
 @RestController
 @RequestMapping("api/var")
 public class PathVariableController {
+
+    @Value("${config.username}")
+    private String username;
+
+    //@Value("${config.message}")
+    //private String message;
+
+    @Value("${config.listOfValues}")
+    private String[] listOfValues;
+
+    @Value("${config.code}")
+    private Integer code;
+
     @GetMapping("/baz/{message}")
     public ParamDto baz(@PathVariable String message){
 
@@ -45,6 +59,16 @@ public class PathVariableController {
         user.setName(user.getName().toUpperCase());
         return user;
 
+    }
+
+    @GetMapping("/values")
+    public Map<String, Object> values(@Value("${config.message}") String message){
+        Map<String, Object> json = new HashMap<>();
+        json.put("username", username);
+        json.put("message", message);
+        json.put("listOfValues", listOfValues);
+        json.put("code", code);
+        return json;
     }
 
 
